70aa6744fe3301c5f2927499b2b261fce8e36426
noone <noone@example.com>
2023-07-18 17:13:59 +0800

    add example

--- example/Dice.cs
+++ example/Dice.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Numerics;
+using Neo;
+using Neo.SmartContract;
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
 
 namespace example
 {
@@ -12,9 +18,33 @@ namespace example
     [ManifestExtra("Description", "This is a example")]
     public class example : SmartContract
     {
-        public static bool Main()
+        [DisplayName("Win")]   // account
+        public static event Action<UInt160> Win;
+
+        public struct OraclePayload
         {
-            return true;
+            public UInt256 hashkey;
+            public object data;
+            public ulong timestamp;
+        }
+
+        public static readonly UInt256 HASHKEY = (UInt256)CryptoLib.Sha256("RANDOM");
+        public static readonly BigInteger MOD = 1048576;
+
+
+        [InitialValue("[TODO]: ARGS", ContractParameterType.Hash160)]
+        private static readonly UInt160 R3E = default;
+
+        // must be two parameters, the first is the account verified by R3E, the second is an array accept all other needed args
+        public static void Play(UInt160 account, object[] args)
+        {
+            OraclePayload data = (OraclePayload)Contract.Call(R3E, "data", CallFlags.ReadOnly, HASHKEY);
+            ExecutionEngine.Assert(data.hashkey == HASHKEY, "oracle record not exist");
+            ExecutionEngine.Assert(data.timestamp > (BigInteger)Runtime.Time + 30, "oracle record exipred");
+
+            if ((BigInteger)CryptoLib.Sha256((ByteString)data.data) % MOD == 0) {
+                Win(account);
+            }
         }
     }
 }
