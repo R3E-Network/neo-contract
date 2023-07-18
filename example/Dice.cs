c91392b79085b830fe39c27ce079b3ed8d8eb979
noone <noone@example.com>
2023-07-18 16:56:11 +0800

    reorg

---
+++ example/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+
+namespace example
+{
+    [DisplayName("example")]
+    [ManifestExtra("Author", "NEO")]
+    [ManifestExtra("Email", "developer@neo.org")]
+    [ManifestExtra("Email", "developer@neo.org")]
+    [ManifestExtra("Description", "This is a example")]
+    public class example : SmartContract
+    {
+        public static bool Main()
+        {
+            return true;
+        }
+    }
+}
